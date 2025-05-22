46e39482b21f63c9f3d801a1ee381be8c785fb1f
Julia Martins <juliamartins.td2.senai@gmail.com>
2025-05-22 10:53:34 -0300

    feat: finished auth for front-and

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "./context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,9 +19,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+       <AuthProvider>
         {children}
+        </AuthProvider>
       </body>
     </html>
   );
